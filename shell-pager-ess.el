e13ab16d1d58ebf53aa0d3d05a4d91639e963140
Joel Lööw <jloow89@gmail.com>
2024-12-12 14:19:27 +0100

    Add author and license

--- shell-pager-ess.el
+++ shell-pager-ess.el
@@ -1,5 +1,21 @@
 ;;; shell-pager-ess.el --- Support for ESS  -*- lexical-binding: t -*-
 
+;; Author: Joel Lööw <joel@joelloow.se>
+
+;;; License
+;;
+;; This package is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This package is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs. If not, see https://www.gnu.org/licenses/.
 
 ;;; Commentary:
 ;;
